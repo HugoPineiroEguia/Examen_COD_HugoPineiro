1899c54954973fe582ba711c33a9b667f4f0a35b
Hugo Pineiro Eguia <hpineiroeguia@danielcastelao.org>
2022-03-16 13:26:32 +0100

    Primer contacto con la nueva API, todo mal

--- src/main/java/Mainclass.java
+++ src/main/java/Mainclass.java
@@ -1,5 +1,21 @@
 public class Mainclass {
     public static void main(String[] args) {
+        final String token = args[0];
+        final DiscordClient client = DiscordClient.create(token);
+        final GatewayDiscordClient gateway = client.login().block();
+
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            final Message message = event.getMessage();
+            if ("!ping".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                channel.createMessage("Pong!").block();
+            }
+        });
+
+        gateway.onDisconnect().block();
+    }
+}
+
 
     }
 }
