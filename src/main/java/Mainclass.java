ddeda5acb3a53c3ff231865bb38103c461a37a4f
Hugo Pineiro Eguia <hpineiroeguia@danielcastelao.org>
2022-03-16 13:41:31 +0100

    Implementacion del token en el codigo

--- src/main/java/Mainclass.java
+++ src/main/java/Mainclass.java
@@ -6,7 +6,7 @@ import discord4j.core.object.entity.channel.MessageChannel;
 
 public class Mainclass {
     public static void main(String[] args) {
-        final String token = args[0];
+        final String token = "d1447ce73859ba6b39bb1b7a213ce2e99bd330c697c8868d9f20a8c0a3714e1b";
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
